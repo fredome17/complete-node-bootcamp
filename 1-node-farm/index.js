0a1838e7c50b61c860b98c7258bbb7769d126462
Yulizar Widiatama <yulizarwidiatama@gmail.com>
2020-01-08 16:06:42 +0700

    simplenode

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,27 @@
 const fs = require('fs');   
 
+//Blocking, synchronous way
+// const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
+// console.log(textIn);
+
+// const textOut = `This is what we know about avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./starter/txt/output.txt', textOut);
+// console.log('File Written!!');
 
 
-const hello = 'Hello World';
-console.log(hello);
+//non Blocking, asynchronous way
+fs.readFile('./starter/txt/start.txt', 'utf-8',(err, data1) => {
+    if (err) return console.log('Error !! Duaar');
+    fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile(`./starter/txt/append.txt`,'utf-8',(err, data3) => {
+            console.log(data3);
+
+            fs.writeFile(`./starter/txt/final.txt`, `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written :)')
+            })
+        })
+    })
+})
+
+console.log('Will Read File');
