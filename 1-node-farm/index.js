2e86d21b4e01099eb439366584652bdc9129c953
Yulizar Widiatama <yulizarwidiatama@gmail.com>
2020-01-10 10:37:15 +0700

    createserver

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,9 @@
 const fs = require('fs');   
+const http = require ('http');
+const url = require ('url');
 
+
+/////// Files 
 //Blocking, synchronous way
 // const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -10,18 +14,38 @@ const fs = require('fs');
 
 
 //non Blocking, asynchronous way
-fs.readFile('./starter/txt/start.txt', 'utf-8',(err, data1) => {
-    if (err) return console.log('Error !! Duaar');
-    fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile(`./starter/txt/append.txt`,'utf-8',(err, data3) => {
-            console.log(data3);
+// fs.readFile('./starter/txt/start.txt', 'utf-8',(err, data1) => {
+//     if (err) return console.log('Error !! Duaar');
+//     fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile(`./starter/txt/append.txt`,'utf-8',(err, data3) => {
+//             console.log(data3);
 
-            fs.writeFile(`./starter/txt/final.txt`, `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('Your file has been written :)')
-            })
-        })
-    })
-})
+//             fs.writeFile(`./starter/txt/final.txt`, `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('Your file has been written :)')
+//             })
+//         })
+//     })
+// })
 
-console.log('Will Read File');
+// console.log('Will Read File');
+
+/////// Server
+const server = http.createServer((req,res) => {
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview'){
+        res.end('This is an OVERVIEW');
+    } else if (pathName === '/product'){
+    res.end('This is a PRODUCT');
+    } else{
+        res.writeHead(404, {
+            'Content-type':'text/html',
+            'my-own-header': 'hello world'
+        });
+        res.end('<h1>Page not Found</h1>');
+    }
+});
+
+server.listen(3300, '127.0.0.1', () =>{
+    console.log('listening to the request on port 3300');
+});
