1403edf96c7f923c8dc1eaaeec7d88cb3a6c6100
Yulizar Widiatama <yulizarwidiatama@gmail.com>
2020-01-15 13:59:16 +0700

    isi

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -31,23 +31,44 @@ const url = require ('url');
 // console.log('Will Read File');
 
 /////// Server
+const tempOverview =fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`,`utf-8`);
+const tempCard =fs.readFileSync(`${__dirname}/starter/templates/template-card.html`,`utf-8`);
+const tempProduct=fs.readFileSync(`${__dirname}/starter/templates/template-product.html`,`utf-8`);
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output = output.replace (/{%IMAGE%}/g,product.image);
+    output = output.replace (/{%PRICE%}/g,product.price);
+    output = output.replace (/{%FROM%}/g,product.from);
+    output = output.replace (/{%PRODUNUTRIENTS%}/g,product.produNutrients);
+    output = output.replace (/{%QUANTITY%}/g,product.quantity);
+    output = output.replace (/{%DESCRIPTION%}/g,product.description);
+    output = output.replace (/{%ID%}/g,product.id);
+    
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
 const data =fs.readFileSync(`${__dirname}/starter/dev-data/data.json`,`utf-8`);
 const dataObj = JSON.parse(data);
 
-
-
 const server = http.createServer((req,res) => {
     const pathName = req.url;
+
+    //overview page
     if (pathName === '/' || pathName === '/overview'){
-        res.end('This is an OVERVIEW');
-
+        res.writeHead(200, {'Content-type':'text/html'});
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el))
+        res.end(tempOverview);
+    
+    // product page
     } else if (pathName === '/product'){
-    res.end('This is a PRODUCT');
-
+        res.end('product');
+    
+    //api
     } else if (pathName === '/api'){
         res.writeHead(200, {'Content-type':'application/json'});
         res.end(data);
-
+    // not found        
     }else{
         res.writeHead(404, {
             'Content-type':'text/html',
